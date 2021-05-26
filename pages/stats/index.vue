fdfd43e7ab64c786d671caab09393f885d234715
Hamish <hamish@swipedon.com>
2021-05-26 21:29:38 +1200

    init

---
+++ pages/stats/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <h2>Stats</h2>
+    </v-col>
+  </v-row>
+</template>
